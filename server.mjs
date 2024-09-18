91259c355f68b9ba4f03d1801d674fb053888c49
reinharn <nate.reinhardt@wwt.com>
2024-09-18 17:24:12 -0500

    it works

--- server.mjs
+++ server.mjs
@@ -1,14 +1,27 @@
 // server.mjs
-import { createServer } from 'node:http';
+import { createServer } from 'node:http'
+import { main as runOauthFlow } from './eve-esi/esiOauthNative.mjs'
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
+  if (req.url === '/run-oauth' && req.method === 'GET') {
+    runOauthFlow()
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('OAuth flow completed successfully.\n')
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end(`Error during OAuth flow: ${error.message}\n`)
+      })
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Not Found\n')
+  }
+})
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
+  console.log('Listening on 127.0.0.1:3000')
+})
 
 // run with `node server.mjs`
