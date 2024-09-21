e50c7514ca4ee8fecb5cfa9800bd6a0efc4f267c
Nate R <natemreinhardt@gmail.com>
2024-09-21 01:18:09 -0500

    toned down alot

--- eve-esi/esiOauthNative.mjs
+++ eve-esi/esiOauthNative.mjs
@@ -1,43 +1,49 @@
-import crypto from 'crypto';
-import readline from 'readline';
+import crypto from "crypto";
+import readline from "readline";
 import fetch from "node-fetch";
 import { validateEveJwt } from "./validateJwt.mjs";
 import { URLSearchParams } from "url";
+import { sendToTinybird } from "../tinybird/tinyBirdService.mjs";
 
 export async function main() {
-  console.log("Takes you through a local example of the OAuth 2.0 native flow.");
+  console.log(
+    "Takes you through a local example of the OAuth 2.0 native flow."
+  );
 
   // Generate a code verifier and code challenge for PKCE
-  const codeVerifier = crypto.randomBytes(32).toString('hex');
-  const codeChallenge = crypto.createHash('sha256').update(codeVerifier).digest('base64url');
+  const codeVerifier = crypto.randomBytes(32).toString("hex");
+  const codeChallenge = crypto
+    .createHash("sha256")
+    .update(codeVerifier)
+    .digest("base64url");
 
   // Print the authorization URL
-  const clientId = '7e42742a49e449c190b57ee5ba4d1a3b'; // Replace with your actual client ID
+  const clientId = "7e42742a49e449c190b57ee5ba4d1a3b"; // Replace with your actual client ID
   printAuthUrl(clientId, codeChallenge);
 
   // Prompt the user to enter the authorization code
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
 
-  rl.question('Enter the authorization code: ', async (authCode) => {
+  rl.question("Enter the authorization code: ", async (authCode) => {
     rl.close();
 
     // Send a request for an authorization token
     const formValues = {
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code: authCode,
       client_id: clientId,
       code_verifier: codeVerifier,
-      redirect_uri: 'https://localhost/callback/' // Replace with your actual redirect URI
+      redirect_uri: "https://localhost/callback/", // Replace with your actual redirect URI
     };
 
     try {
       const ssoResponse = await sendTokenRequest(formValues);
       await handleSsoTokenResponse(ssoResponse);
     } catch (error) {
-      console.error('Error during the OAuth 2.0 flow:', error);
+      console.error("Error during the OAuth 2.0 flow:", error);
     }
   });
 }
@@ -215,79 +221,5 @@ function sanitizeEntry(entry) {
     }
   }
 }
-
-async function sendToTinybird(data) {
-  const tinybirdUrl =
-    "https://api.us-east.tinybird.co/v0/events?name=S0b_Wallet_ESI";
-  const tinybirdToken =
-    "Bearer p.eyJ1IjogIjdiYmRhODI4LTA4ZDItNGM0Yi04YmRkLTNkZjk4ZWZjM2JhOSIsICJpZCI6ICIwMjRmYWFmZC1lNjVhLTQ3MWQtYWNjMC0xNjUwODAyMGZkMjEiLCAiaG9zdCI6ICJ1c19lYXN0In0.K5FxrcUgU6f5XDLp9xo4bFfuQUofSz03wU33Tpa8tJE";
-
-  // Convert data to NDJSON format
-  const ndjsonData = data.map((entry) => JSON.stringify(entry)).join("\n");
-
-  try {
-    const res = await fetch(tinybirdUrl, {
-      method: "POST",
-      body: ndjsonData,
-      headers: {
-        "Content-Type": "application/x-ndjson",
-        Authorization: tinybirdToken,
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error(`Failed to send data to Tinybird: ${res.statusText}`);
-    }
-
-    const responseData = await res.json();
-    console.info("Data sent to Tinybird:", responseData);
-  } catch (error) {
-    console.error("Error sending data to Tinybird:", error);
-  }
-}
-
-async function initTinybird() {
-  const tinybirdUrl =
-    "https://api.us-east.tinybird.co/v0/events?name=S0b_Wallet_ESI";
-  const tinybirdToken =
-    "Bearer p.eyJ1IjogIjdiYmRhODI4LTA4ZDItNGM0Yi04YmRkLTNkZjk4ZWZjM2JhOSIsICJpZCI6ICIwMjRmYWFmZC1lNjVhLTQ3MWQtYWNjMC0xNjUwODAyMGZkMjEiLCAiaG9zdCI6ICJ1c19lYXN0In0.K5FxrcUgU6f5XDLp9xo4bFfuQUofSz03wU33Tpa8tJE";
-
-  const data = {
-    amount: parseFloat(58320).toFixed(2),
-    balance: parseFloat(9798963512.8173).toFixed(2),
-    context_id: 40154468,
-    context_id_type: "planet_id",
-    date: "2024-09-18T15:33:05Z",
-    description:
-      "Planetary Export Tax: SnookPP Zanjoahir exported from M-MD31 II",
-    first_party_id: 2114715198,
-    id: 23289030664,
-    reason: "Export Duty for M-MD31 II",
-    ref_type: "planetary_export_tax",
-    second_party_id: 98399918,
-    wallet_division: 1,
-  };
-
-  try {
-    const res = await fetch(tinybirdUrl, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: tinybirdToken,
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error(`Failed to send data to Tinybird: ${res.statusText}`);
-    }
-
-    const responseData = await res.json();
-    console.info("Data sent to Tinybird:", responseData);
-  } catch (error) {
-    console.error("Error sending data to Tinybird:", error);
-  }
-}
-
 
 main().catch(console.error);
