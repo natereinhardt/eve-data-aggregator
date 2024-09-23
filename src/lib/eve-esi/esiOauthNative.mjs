b86c19d9d6de9338c6b823b0e0b864e3924ae951
Nate R <natemreinhardt@gmail.com>
2024-09-22 23:00:13 -0500

    it works

--- src/lib/eve-esi/esiOauthNative.mjs
+++ src/lib/eve-esi/esiOauthNative.mjs
@@ -7,7 +7,9 @@ import { validateEveJwt } from "./validateJwt.mjs";
 import { sendToTinybird } from "../tinyBird/tinyBirdService.mjs";
 
 export async function runOAuthFlow() {
-  console.log("Takes you through a local example of the OAuth 2.0 native flow.");
+  console.log(
+    "Takes you through a local example of the OAuth 2.0 native flow."
+  );
 
   const { codeVerifier, codeChallenge } = generateCodeVerifierAndChallenge();
 
@@ -130,68 +132,8 @@ export async function handleSsoTokenResponse(ssoResponse) {
 
     const jwt = await validateEveJwt(accessToken);
     console.log(jwt);
-    const characterName = jwt["name"];
 
-    const corporationId = 98399918;
-
-    for (let walletDivision = 1; walletDivision <= 6; walletDivision++) {
-      let page = 1;
-      let hasMorePages = true;
-
-      while (hasMorePages) {
-        const walletPath = `https://esi.evetech.net/latest/corporations/${corporationId}/wallets/${walletDivision}/journal/?page=${page}`;
-
-        console.log(
-          `\nFetching data for wallet division ${walletDivision}, page ${page}...`
-        );
-
-        const headers = {
-          Authorization: `Bearer ${accessToken}`,
-        };
-
-        try {
-          const res = await fetch(walletPath, { headers: headers });
-          console.log(
-            `\nMade request to ${walletPath} with headers: ${JSON.stringify(
-              res.headers.raw()
-            )}`
-          );
-
-          if (res.status === 500) {
-            console.error(
-              `Received 500 error for wallet division ${walletDivision}, page ${page}. Moving to next division.`
-            );
-            hasMorePages = false;
-            continue;
-          }
-
-          if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-          }
-
-          const data = await res.json();
-          if (data.length === 0) {
-            hasMorePages = false;
-          } else {
-            data.forEach((entry) => {
-              entry.wallet_division = walletDivision;
-              entry.amount = parseFloat(entry.amount).toFixed(2);
-              entry.balance = parseFloat(entry.balance).toFixed(2);
-            });
-            console.log(
-              `\n${characterName} has ${data.length} wallet journal entries in division ${walletDivision}, page ${page}`
-            );
-            await sendToTinybird(data);
-            page++;
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching data for wallet division ${walletDivision}, page ${page}: ${error.message}`
-          );
-          hasMorePages = false;
-        }
-      }
-    }
+    await importWalletData(jwt, accessToken);
   } else {
     console.log(
       "\nSomething went wrong! Re read the comment at the top of this file and make sure you completed all the prerequisites then try again. Here's some debug info to help you out:"
@@ -199,9 +141,73 @@ export async function handleSsoTokenResponse(ssoResponse) {
     console.log(
       `\nSent request with url: ${ssoResponse.url} \nbody: ${
         ssoResponse.body
-      } \nheaders: ${JSON.stringify(res.headers.raw())}`
+      } \nheaders: ${JSON.stringify(ssoResponse.headers.raw())}`
     );
     console.log(`\nSSO response code is: ${ssoResponse.status}`);
     console.log(`\nSSO response JSON is: ${await ssoResponse.json()}`);
+  }
+}
+
+export async function importWalletData(jwt, accessToken) {
+  const characterName = jwt["name"];
+  const corporationId = 98399918;
+
+  for (let walletDivision = 1; walletDivision <= 6; walletDivision++) {
+    let page = 1;
+    let hasMorePages = true;
+
+    while (hasMorePages) {
+      const walletPath = `https://esi.evetech.net/latest/corporations/${corporationId}/wallets/${walletDivision}/journal/?page=${page}`;
+
+      console.log(
+        `\nFetching data for wallet division ${walletDivision}, page ${page}...`
+      );
+
+      const headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+
+      try {
+        const res = await fetch(walletPath, { headers: headers });
+        console.log(
+          `\nMade request to ${walletPath} with headers: ${JSON.stringify(
+            res.headers.raw()
+          )}`
+        );
+
+        if (res.status === 500) {
+          console.error(
+            `Received 500 error for wallet division ${walletDivision}, page ${page}. Moving to next division.`
+          );
+          hasMorePages = false;
+          continue;
+        }
+
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        if (data.length === 0) {
+          hasMorePages = false;
+        } else {
+          data.forEach((entry) => {
+            entry.wallet_division = walletDivision;
+            entry.amount = parseFloat(entry.amount).toFixed(2);
+            entry.balance = parseFloat(entry.balance).toFixed(2);
+          });
+          console.log(
+            `\n${characterName} has ${data.length} wallet journal entries in division ${walletDivision}, page ${page}`
+          );
+          await sendToTinybird(data);
+          page++;
+        }
+      } catch (error) {
+        console.error(
+          `Error fetching data for wallet division ${walletDivision}, page ${page}: ${error.message}`
+        );
+        hasMorePages = false;
+      }
+    }
   }
 }
