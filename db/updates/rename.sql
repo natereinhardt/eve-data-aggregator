36da9c64a6674e4f6c6f9e68c64b9789832669eb
Nate R <natemreinhardt@gmail.com>
2025-01-28 10:33:06 -0600

    wallet rename

---
+++ db/updates/rename.sql
@@ -0,0 +1,20 @@
+-- Update ID 2 to "S0b Salary Account"
+UPDATE S0b.wallets
+SET Name = 'S0b Salary Account'
+WHERE ID = 2;
+
+-- Update ID 3 to "S0b Expense Account"
+UPDATE S0b.wallets
+SET Name = 'S0b Expense Account'
+WHERE ID = 3;
+
+-- Update ID 6 to "S0b Alliance Dues"
+UPDATE S0b.wallets
+SET Name = 'S0b Alliance Dues'
+WHERE ID = 6;
+
+-- Update ID 7 to "Debit Account"
+UPDATE S0b.wallets
+SET Name = 'Debit Account'
+WHERE ID = 7;
+
